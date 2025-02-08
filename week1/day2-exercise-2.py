135647002d4eef915071802f5fdcf036ba2a314b
Bona Nugroho <bona.nugroho@gmail.com>
2025-02-08 10:13:23 +0700

    stream on ollama api

--- week1/day2-exercise-2.py
+++ week1/day2-exercise-2.py
@@ -1,7 +1,6 @@
 import ollama
 
-OLLAMA_API = "http://localhost:11434/api/chat"
-OPENAI_OLLAMA_API = "http://localhost:11434/v1"
+OLLAMA_API = "http://localhost:11434"
 MODEL_LLAMA_32 = "llama3.2:3b"
 MODEL_QWEN_25 = "qwen2.5:3b"
 MODEL_DEEPSEEK_R1_1 = "deepseek-r1:1.5b"
@@ -11,7 +10,7 @@ messages = [
     {"role": "user", "content": "Describe some of the business applications of Generative AI"}
 ]
 
-system_message = "You are an assistant that is great at telling jokes"
+system_message = "You are an assistant that is great at telling jokes and responds in markdown"
 user_prompt = "Tell a light-hearted joke for an audience of Data Scientists"
 
 prompts = [
@@ -19,8 +18,21 @@ prompts = [
     {"role": "user", "content": user_prompt}
   ]
 
-# response = ollama.chat(model=MODEL_QWEN_25, messages=messages)
-response = ollama.chat(model=MODEL_LLAMA_32, messages=prompts)
+# client = ollama.Client(host=OLLAMA_API)
 
-print(response['message']['content'])
-print(f"Using Model: ", response.model)
+# response = ollama.chat(model=MODEL_QWEN_25, messages=messages)
+# response = client.chat(model=MODEL_LLAMA_32, messages=prompts)
+
+# print(response['message']['content'])
+
+
+stream = ollama.chat(
+    model=MODEL_LLAMA_32,
+    messages=prompts,
+    stream=True,
+)
+
+for chunk in stream:
+    print(chunk['message']['content'], end='', flush=True)
+
+# print(f"Using Model: ", stream.model)
