6fe65793b66874499f70e3f763d543dc8113bbde
Bona Nugroho <bona.nugroho@gmail.com>
2025-02-14 10:17:02 +0700

    Handling tool call at gpt api

--- week2/day4-01-using-tools.py
+++ week2/day4-01-using-tools.py
@@ -1,7 +1,7 @@
 # imports
-
 import os
 from dotenv import load_dotenv
+import json
 
 from openai import OpenAI
 import anthropic
@@ -38,9 +38,30 @@ system_message = "You are a helpful assistant for an Airline called FlightAI. "
 system_message += "Give short, courteous answers, no more than 1 sentence. "
 system_message += "Always be accurate. If you don't know the answer, say so."
 
+def handle_tool_call_gpt(message):
+    tool_call = message.tool_calls[0]
+    arguments = json.loads(tool_call.function.arguments)
+    city = arguments.get('destination_city')
+    price = get_ticket_price(city)
+    response = {
+        "role": "tool",
+        "content": json.dumps({"destination_city": city, "price": price}),
+        "tool_call_id": tool_call.id,
+    }
+
+    return response, city
+
 def chat_gpt(message, history):
     messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
-    response = openai.chat.completions.create(model=MODEL_GPT, messages=messages)
+    response = openai.chat.completions.create(model=MODEL_GPT, messages=messages, tools=tools)
+
+    if response.choices[0].finish_reason == "tool_calls" :
+        message = response.choices[0].message
+        response, city = handle_tool_call_gpt(message)
+        messages.append(message)
+        messages.append(response)
+        response = openai.chat.completions.create(model=MODEL_GPT, messages=messages)
+
     return response.choices[0].message.content
 
 def chat_claude(message, history):
@@ -83,4 +104,43 @@ def stream_by_model(message, history, model):
 
     return response
 
+# Let's start by making a useful function
+ticket_prices = {"london": "$799", "paris": "$899", "tokyo": "$1400", "berlin": "$499"}
 
+def get_ticket_price(destination_city):
+    print(f"Tool get_ticket_price called for {destination_city}")
+    city = destination_city.lower()
+    return ticket_prices.get(city, "Unknown")
+
+
+# There's a particular dictionary structure that's required to describe our function:
+price_function = {
+    "name": "get_ticket_price",
+    "description": "Get the price of a return ticket to the destination city. Call this whenever you need to know the ticket price, for example when a customer asks 'How much is a ticket to this city'",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "destination_city": {
+                "type": "string",
+                "description": "The city that the customer wants to travel to",
+            },
+        },
+        "required": ["destination_city"],
+        "additionalProperties": False
+    }
+}
+
+# And this is included in a list of tools:
+tools = [{"type": "function", "function": price_function}]
+
+
+with gr.Blocks() as ui:
+    select_model = gr.Dropdown(["GPT", "Claude", "llama3.2:3b", "qwen2.5:3b", "gemma2:2b", "phi3:3.8b"], label="Select model", value="GPT")
+
+    gr.ChatInterface(
+        fn=stream_by_model,
+        type="messages",
+        additional_inputs=[select_model],
+    )
+
+ui.launch()
